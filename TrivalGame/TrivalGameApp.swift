3a77b4f3e1a761c7fba167c7537d0914684b3a28
mobinezzati <mobinezzati662@gmail.com>
2025-03-14 15:07:35 -0500

    Initial Commit

---
+++ TrivalGame/TrivalGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrivalGameApp.swift
+//  TrivalGame
+//
+//  Created by Mobin  Ezzati  on 3/14/25.
+//
+
+import SwiftUI
+
+@main
+struct TrivalGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
